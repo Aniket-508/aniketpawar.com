884749a69bad53258809a006b9283f10f226f15e
Aniket-508 <pawaraniket508@gmail.com>
2025-01-18 22:07:38 +0530

    chore: updated project tags

--- lib/projects.ts
+++ lib/projects.ts
@@ -16,21 +16,21 @@ export const getProjects = function (): ProjectItemProps[] {
         githubURL: "https://github.com/Aniket-508/instagram-posts-generator",
         liveURL: "https://instagram-posts-generator.vercel.app/",
       },
-      category: ["ui/ux"],
-    },
-    {
-      projectTitle: "Typeform Clone",
-      projectData: {
-        githubURL: "https://github.com/Aniket-508/Typeform-Clone",
-        liveURL: "https://typeform-clone-aniket-508.vercel.app/",
-      },
-      category: ["ui/ux"],
+      category: ["ui/ux", "full-stack"],
     },
     {
       projectTitle: "GitHub Business Card",
       projectData: {
         githubURL: "https://github.com/Aniket-508/github-business-card",
         liveURL: "https://gh-business-card.vercel.app/",
+      },
+      category: ["ui/ux", "full-stack"],
+    },
+    {
+      projectTitle: "Typeform Clone",
+      projectData: {
+        githubURL: "https://github.com/Aniket-508/Typeform-Clone",
+        liveURL: "https://typeform-clone-aniket-508.vercel.app/",
       },
       category: ["ui/ux"],
     },
