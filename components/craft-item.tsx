6423846851dabca0bc2b191f60963d2ec071cf7c
Aniket-508 <pawaraniket508@gmail.com>
2026-06-11 16:28:40 +0530

    chore: fix build issue

--- components/craft-item.tsx
+++ components/craft-item.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link } from "next/link";
+import Link from "next/link";
 
 import { MediaPreview } from "@/components/media-preview";
 import { ROUTES } from "@/constants/routes";
