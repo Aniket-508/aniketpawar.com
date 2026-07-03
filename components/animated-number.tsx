3051bfedb093611712fe22f2fe97e2967aa7e859
Aniket-508 <pawaraniket508@gmail.com>
2026-07-03 23:21:17 +0530

    refactor: simplify AnimatedNumber component by removing props interface and spreading NumberFlow props

--- components/animated-number.tsx
+++ components/animated-number.tsx
@@ -2,29 +2,6 @@
 
 import NumberFlow from "@number-flow/react";
 
-interface AnimatedNumberProps {
-  value: number;
-  format?: Intl.NumberFormatOptions;
-  locales?: string | string[];
-  prefix?: string;
-  suffix?: string;
-  className?: string;
-}
-
 export const AnimatedNumber = ({
-  value,
-  format,
-  locales = "en-US",
-  prefix,
-  suffix,
-  className,
-}: AnimatedNumberProps) => (
-  <NumberFlow
-    value={value}
-    format={format}
-    locales={locales}
-    prefix={prefix}
-    suffix={suffix}
-    className={className}
-  />
-);
+  ...props
+}: React.ComponentProps<typeof NumberFlow>) => <NumberFlow {...props} />;
